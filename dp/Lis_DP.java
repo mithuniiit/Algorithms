27d71d8cf7ccbff96efdcea0e16cd95cb3c8be0a
mithuniiit <mithunsingh@gmail.com>
2020-01-17 04:59:57 +0530

    Create Lis_DP.java

---
+++ dp/Lis_DP.java
@@ -0,0 +1,32 @@
+package dp;
+
+public class Lis_DP {
+	public static void main(String[] args) {
+		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
+        int n = arr.length; 
+        System.out.println("Length of lis is " + findLis( arr, n ) + "\n" ); 
+	}
+
+	private static int findLis(int[] arr, int n) {
+		int lis[] = new int[n];
+		for(int i=0;i<n;i++) {
+			lis[i]=1;
+		}
+		
+		for(int i=1;i<n;i++) {
+			for(int j=0;j<i;j++) {
+				if(arr[j] < arr[i] && lis[i] < lis[j]+1) {
+					lis[i] = lis[j]+1;
+				}
+			}
+		}
+		
+		int max=0;
+		for(int i=0;i<n;i++) {
+			if(max < lis[i])
+				max = lis[i];
+		}
+		
+		return max;
+	}
+}
