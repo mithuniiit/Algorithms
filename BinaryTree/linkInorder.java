12a5663cf7eeeff6186a3a06aa09527e7be977bb
mithuniiit <mithunsingh@gmail.com>
2020-04-09 21:41:11 +0530

    Create linkInorder.java

---
+++ BinaryTree/linkInorder.java
@@ -0,0 +1,39 @@
+package BinaryTree;
+
+public class InorderSuccesor {
+	
+	static Node root;
+	
+	static class Node{
+		int data;
+		Node left;
+		Node right;
+		Node next;
+		Node(int data){
+			this.data = data;
+		}
+	}
+	
+	private static void linkInorder(Node root, Node next) {
+		if(root == null)
+			return;
+		
+		linkInorder(root.right, next);
+		root.next = next;
+		next = root;
+		linkInorder(root.left, next);
+	}
+	
+	
+	public static void main(String[] args) {
+		root = new Node(1);
+		root.left = new Node(2);
+		root.right = new Node(3);
+		root.left.left = new Node(4);
+		root.left.right = new Node(5);
+		root.right.left = new Node(6);
+		root.right.right = new Node(7);
+		Node next = null;
+		linkInorder(root,next);
+	}
+}
