510f2a5f8c695126aaaf6fcd49e8bdf18ba50687
mithuniiit <mithunsingh@gmail.com>
2020-04-27 23:08:15 +0530

    Add files via upload

--- BinaryTree/summation/FindLargestSubTreeSum.java
+++ BinaryTree/summation/FindLargestSubTreeSum.java
@@ -1,40 +1,33 @@
-package BinaryTree;
+package trees.summation;
 
 public class FindLargestSubTreeSum {
+
+	static int maxSum = Integer.MIN_VALUE;
 	
-	static class Node{
-		int data;
-		Node left;
-		Node right;
-		Node(int data){
-			this.data = data;
-		}
-	}
 	
-	static int maxSum = 0;
-	
-	public static int findSum(Node root) {
+	public static int findMax(Node root, int sum) {
 		if(root == null)
 			return 0;
 		
-		int sum = root.data + findSum(root.left) + findSum(root.right);
+		sum = root.data + findMax(root.left,sum) + findMax(root.right,sum);
 		
 		if(sum > maxSum)
 			maxSum = sum;
 		
-		return sum;
+		return sum; 
 		
 	}
-	
 	public static void main(String[] args) {
-		Node root = new Node(1);
-		root.left = new Node(-2);
-		root.right = new Node(3);
-		root.left.left = new Node(4);
-		root.left.right = new Node(5);
-		root.right.left = new Node(-6);
-		root.right.right = new Node(2);
-		System.out.println(findSum(root));
+		Node root = new Node(1); 
+        root.left = new Node(-2); 
+        root.right = new Node(3); 
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(-6);
+        root.right.right = new Node(2);
+        
+        findMax(root,0);
+        
+        System.out.println(maxSum);
 	}
-
 }
