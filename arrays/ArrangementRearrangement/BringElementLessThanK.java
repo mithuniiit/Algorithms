f78f4d7d9af55e26b3f5b9e877bd6b52fc51a76f
mithuniiit <mithunsingh@gmail.com>
2020-05-25 12:43:12 +0530

    Minimum swaps required to bring all elements less than or equal to k together

--- arrays/ArrangementRearrangement/BringElementLessThanK.java
+++ arrays/ArrangementRearrangement/BringElementLessThanK.java
@@ -22,16 +22,47 @@ public class BringElementLessThanK {
 			swap(a, i, j);
 		}
 	}
+	
+	
+	public static void findMin(int[]a, int n, int k) {
+		
+		int count = 0;
+		
+		for(int i=0; i<n;i++) {
+			if(a[i] <= k)
+				count++;
+		}
+		
+		int bad = 0;
+		for(int i=0;i<count;i++) {
+			if(a[i] > k)
+				bad++;
+		}
+		
+		int ans = bad;
+		
+		for(int i=0,j=count;j<n;j++,i++) {
+			if(a[i]>k)
+				bad--;
+			
+			if(a[j]>k)
+				bad++;
+			
+			ans = Math.min(ans,bad);
+		}
+		System.out.println(ans);
+	}
 
 	public static void main(String[] args) {
 		int arr[] = {2, 1, 5, 6, 3}; 
 		int n = arr.length; 
 		int k = 3; 
 
-		minSwap(arr,n,k);
+		findMin(arr,n,k);
+		//minSwap(arr,n,k);
 		
-		System.out.println();
-		for(int i=0;i<n;i++)
-			System.out.print(arr[i]);
+//		System.out.println();
+//		for(int i=0;i<n;i++)
+//			System.out.print(arr[i]);
 	}
 }
