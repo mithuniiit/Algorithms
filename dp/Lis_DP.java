5e7d16f316143e574f19049b30f9f8dacc09780d
mithuniiit <mithunsingh@gmail.com>
2020-06-12 15:59:31 +0530

    Update Lis_DP.java

--- dp/Lis_DP.java
+++ dp/Lis_DP.java
@@ -2,29 +2,27 @@ package dp;
 
 public class Lis_DP {
 	public static void main(String[] args) {
-		int arr[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
-        int n = arr.length; 
-        System.out.println("Length of lis is " + findLis( arr, n ) + "\n" ); 
+		int a[] = { 10, 22, 9, 33, 21, 50, 41, 60 }; 
+        int n = a.length; 
+        System.out.println("Length of lis is " + findLis( a, n ) + "\n" ); 
 	}
 
-	private static int findLis(int[] arr, int n) {
-		int lis[] = new int[n];
+	private static int findLis(int[] a, int n) {
+		int []lis = new int[n];
+		
 		for(int i=0;i<n;i++) {
-			lis[i]=1;
+			lis[i] = 1;
 		}
 		
-		for(int i=1;i<n;i++) {
-			for(int j=0;j<i;j++) {
-				if(arr[j] < arr[i] && lis[i] < lis[j]+1) {
-					lis[i] = lis[j]+1;
+		int max = 0;
+		
+		for(int i=1; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				if(a[i] > a[j] && lis[i] < i + lis[j]) {
+					lis[i] = 1 + lis[j];
+					max = Math.max(lis[i],max);
 				}
 			}
-		}
-		
-		int max=0;
-		for(int i=0;i<n;i++) {
-			if(max < lis[i])
-				max = lis[i];
 		}
 		
 		return max;
