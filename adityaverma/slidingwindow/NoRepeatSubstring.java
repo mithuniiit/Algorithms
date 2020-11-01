a1a7d41c5e6ec14f2bb3d818c62a66f9f6dbe6bc
mithuniiit <mithunsingh@gmail.com>
2020-11-01 17:30:29 +0530

    Add files via upload

--- adityaverma/slidingwindow/NoRepeatSubstring.java
+++ adityaverma/slidingwindow/NoRepeatSubstring.java
@@ -18,8 +18,9 @@ class NoRepeatSubstring {
   }
 
   public static void main(String[] args) {
-    System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("aabccbb"));
-    System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("abbbb"));
-    System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("abccde"));
+//    System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("aabccbb"));
+//    System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("abbbb"));
+//    System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("abccde"));
+    System.out.println("Length of the longest substring: " + NoRepeatSubstring.findLength("abcded"));
   }
 }
